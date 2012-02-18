34a948052c61614348807d6cec1147c05e883a18
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-02-18 10:37:01 -0500

    Added HTML framework to display tabs.

---
+++ library/extensions/tabbedcontent.php
@@ -0,0 +1,23 @@
+<ul id="tabs">
+	<li id="tab-content-recent" class="active-tab">
+		<a href="javascript:viewTab('content-recent');">Recent</a>
+	</li>
+	<li id="tab-content-popular">
+		<a href="javascript:viewTab('content-popular');">Popular</a>
+	</li>
+	<li id="tab-content-comments">
+		<a href="javascript:viewTab('content-comments');">Comments</a>
+	</li>
+</ul>
+<div id="contents-container">
+	<div id="content-recent">
+		Content for the Recent tab.
+	</div>
+	<div id="content-popular" style="display: none;">
+		Content for the Popular tab.
+	</div>
+	<div id="content-comments" style="display: none;">
+		Content for the Comments tab.
+	</div>
+</div>
+	
