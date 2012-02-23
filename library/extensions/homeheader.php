68358d12c55ae7fae152a073d34c75a7826917db
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-02-22 23:54:59 -0500

    Header modifications, establishing structure for formating.

--- library/extensions/homeheader.php
+++ library/extensions/homeheader.php
@@ -13,6 +13,23 @@
 	$catSubArray = explode(",",$header_cat_subtitles);
 	$catSubCount = count($catSubArray); 
 		$k=0;
+		
+?>
+
+<div id="home-button-container">
+	<div id="home-button-one">
+		<a href="https://www.voterinfo.sbe.virginia.gov/PublicSite/Public/FT2/PublicLookup.aspx?Link=Registration&amp;AspxAutoDetectCookieSupport=1" target="_blank" onmouseover="MM_swapImage('ltButton','','<?php echo get_stylesheet_directory_uri(); ?>/library/imgs/buttons/ltButtonDown.jpg',1)" onmouseout="MM_swapImgRestore()">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/imgs/buttons/ltButtonUp.jpg" alt="Voting Information" name="ltButton" width="293" height="23" border="0" id="ltButton">
+		</a>
+	</div>
+	<div id="home-button-two">
+		<a href="http://www.sbe.virginia.gov/cms/Voter_Information/Registering_to_Vote/Index.html#Persons" target="_blank" onmouseover="MM_swapImage('rtButton','','<?php echo get_stylesheet_directory_uri(); ?>/library/imgs/buttons/rtButtonDown.jpg',1)" onmouseout="MM_swapImgRestore()">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/imgs/buttons/rtButtonUp.jpg" alt="Register to Vote" name="rtButton" width="297" height="23" border="0" id="rtButton">
+		</a>
+	</div>
+</div>
+<div id="candidates">
+<?php
   
   if ( $header_cats !== "" ){
   
@@ -23,11 +40,14 @@
 
   
 	?>
-		<a href="<?php echo get_category_link( $headerCatArray[$key] ); ?>"><div class="header-category-display">
-			<img src="<?php echo $imgUrlsArray[$key]; ?>" />
-			<h5><?php echo get_cat_name($value); ?></h5>
-			<h6><?php echo $catSubArray[$key]; ?></h6>
-		</div></a>
+	<div>
+		<a href="<?php echo get_category_link( $headerCatArray[$key] ); ?>">
+			<div class="header-category-display" style="background:url('<?php echo $imgUrlsArray[$key]; ?>') top left no-repeat;">
+					<h5><?php echo get_cat_name($value); ?></h5>
+					<h6><?php echo $catSubArray[$key]; ?></h6>
+			</div>
+		</a>
+	</div>
 	<?php
 	
 		$k++;
@@ -37,3 +57,4 @@
   }  
   
 ?>
+</div>
